e39edd7a9b8225720a6d9bef3735a9f56cb1e08d
spardha-testing <139978783+spardha-testing@users.noreply.github.com>
2023-07-20 12:27:18 +0530

    Database env added

--- Spardha/Spardha/settings.py
+++ Spardha/Spardha/settings.py
@@ -95,11 +95,16 @@ WSGI_APPLICATION = "Spardha.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-       'ENGINE': 'django.db.backends.postgresql',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': os.environ.get('DB_NAME'),
+    'HOST': os.environ.get('DB_HOST'),
+    'PORT': os.environ.get('DB_PORT'),
+    'USER': os.environ.get('DB_USER'),
+    'PASSWORD': os.environ.get('DB_PASSWORD'),
+    'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
+  }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
